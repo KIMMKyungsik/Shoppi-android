61ee5e470823b3859cb973391d2af79e5e79c08b
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-03-12 04:37:55 +0900

    바텀네비게이션 뷰 구현

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,26 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botttomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        botttomNavigationView.itemIconTintList = null
+
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+
+            botttomNavigationView.setupWithNavController(it)
+        }
+
+
+
     }
 }
