36ceb2686dfa6378c1008bbb4921653f0fb99d7b
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-05-30 19:11:20 +0900

    카테고리 상세 인기상품 목록 화면

--- app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
@@ -6,10 +6,21 @@ import com.shoppi.app.GlideApp
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
-    if(!imageUrl.isNullOrEmpty()){
+    if (!imageUrl.isNullOrEmpty()) {
         GlideApp.with(view)
             .load(imageUrl)
             .into(view)
     }
 
+}
+
+@BindingAdapter("circleImageUrl")
+
+fun loadCircleImage(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
+            .into(view)
+    }
 }
